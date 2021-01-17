1b6725ebe38c40f678f998988ff386a0c9f08e49
juanaves <juanaves@gmail.com>
2021-01-17 13:42:23 +0100

    Update HomeController.java

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//Controladores
+//Para manejar peticiones Web es necesario crear controladores, los cuales manejan la solicitud
+//a partir de una ruta de acceso. Creamos la clase ControllerIndex en el paquete com.proyecto.controller. 
+//Agregamos las anotaciones @Controller y @GetMapping, .....
+
+
 @Controller
 public class HomeController {
 
